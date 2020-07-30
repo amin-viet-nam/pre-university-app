974e547e93631092e498630421983d1f114620d4
Minh Nguyen <minhnv02@acb.com.vn>
2020-07-30 16:01:13 +0700

    use local category

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView, Text, View } from 'react-native';
-import { Grid } from '@ant-design/react-native';
 import SkeletonContent from 'react-native-skeleton-content';
 import firebase from '../DataStorages/FirebaseApp';
+import { Flex, WhiteSpace, WingBlank } from '@ant-design/react-native';
+import CardView from 'react-native-cardview';
+
 
 export default class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            categoryList: []
+            categoryList: [{
+                "icon" : "math-compass",
+                "text" : "Toán Học"
+              }, {
+                "icon" : "thermometer-quarter",
+                "text" : "Vật Lý"
+              }, {
+                "icon" : "md-logo-electron",
+                "text" : "Hóa Học"
+              }, {
+                "icon" : "book-open",
+                "text" : "Ngữ Văn"
+              }, {
+                "icon" : "history",
+                "text" : "Lịch Sử"
+              }, {
+                "icon" : "earth-outline",
+                "text" : "Địa Lý"
+              }, {
+                "icon" : "paper-plane",
+                "text" : "Tiếng Anh"
+              }, {
+                "icon" : "human",
+                "text" : "Sinh Học"
+              }, {
+                "icon" : "teach",
+                "text" : "GDCD"
+              }]
         }
     }
 
     componentDidMount() {
-        firebase.database().ref('categories')
-            .once('value', snapshot => {
-                if (snapshot.val()) {
-                    this.setState({
-                        categoryList: snapshot.val()
-                    }); 
-                } else {
-                    this.setState({
-                        categoryList: [{icon: 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png', text: 'Lỗi 500: Chưa cấu hình categories'}]
-                    });                    
-                }
-            });
+        // firebase.database().ref('categories')
+        //     .once('value', snapshot => {
+        //         if (snapshot.val()) {
+        //             this.setState({
+        //                 categoryList: snapshot.val()
+        //             }); 
+        //         } else {
+        //             this.setState({
+        //                 categoryList: [{icon: 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png', text: 'Lỗi 500: Chưa cấu hình categories'}]
+        //             });                    
+        //         }
+        //     });
     }
 
     render() {
@@ -42,12 +71,25 @@ export default class HomeScreen extends React.Component {
                     { width: '50%', height: 200, marginBottom: 6 },
                 ]}
                 >
-                    <Grid
-                        data={categoryList}
-                        columnNum={2}
-                        itemStyle={{ height: 150, width: 150}}
-                        onPress={(_el, index) => alert(index)}
-                        />
+                    <WingBlank style={{ marginBottom: 5 }}>
+                    <Flex>
+                        {categoryList.map((item, index) => {
+                            return (
+                                    <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+                                        <CardView
+                                            cardElevation={2}
+                                            cardMaxElevation={2}
+                                            cornerRadius={5}>
+                                            <MaterialCommunityIcons name={item.icon}/>
+                                            <Text>
+                                                {item.text}
+                                            </Text>
+                                        </CardView>
+                                    </Flex.Item>
+                            )
+                        })}
+                    </Flex>
+                    </WingBlank>
                 </SkeletonContent>
             </ScrollView>
           </SafeAreaView>
